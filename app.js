69bbeeb35d74a790551060e17ad4f28371cd3a7c
Sami <samixgames@hotmail.com>
2019-02-13 18:30:28 +0300

    connect MongoDB atlas

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+mongoose.connect('mongodb+srv://Sami:sami123456@wt-cluster-xd7ou.mongodb.net/test?retryWrites=true');
+mongoose.Promise = global.Promise;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
