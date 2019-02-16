41d2d4ba6a9b837c8c679eb5cdf890d354f1bbbe
Sami <samixgames@hotmail.com>
2019-02-16 16:44:49 +0300

    Done alot of thins *check description*
    - Create Model& Schema
    - Create Menu route & connected it
    - create Menu.ejs
    -

---
+++ models/menu.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var uri = 'mongodb+srv://Sami:sami123456@wt-cluster-xd7ou.mongodb.net/test?retryWrites=true'
+mongoose.connect(uri, {dbName: 'wildtiger'});
+mongoose.Promise = global.Promise;
+
+
+var connection = mongoose.connection;
+
+
+var menuSchema = new mongoose.Schema({
+    _id: mongoose.Schema.ObjectId,
+    category: String,
+    food_name: String,
+    food_desc: String,
+    food_price: String
+});
+var Menu = mongoose.model('Menu', menuSchema, 'menu');
+
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', function () {
+    
+    Menu.find(function(err, menus){
+        if(err) return console.err(err);
+        console.log('hey');
+    })
+
+
+    /*
+    connection.db.collection("menu", function(err, collection){
+        collection.find({}).toArray(function(err, data){
+            console.log(JSON.stringify(data)) // it will print your collection data
+        })
+    });
+*/
+});
+module.exports = Menu;
---
+++ routes/getMenu.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var Menu = require('../models/menu')
+var assert = require('assert')
+var mongoose = require('mongoose');
+var connection = mongoose.connection;
+
+var menusJSON, MenuJsoon
+/* GET home page. */
+
+router.get('/menu', function(req, res, next) {
+  Menu.find({}, function (err,menus) {
+    if(err) return console.err(err);
+    menusJSON = JSON.stringify(menus)
+    MenuJsoon = JSON.parse(menusJSON)
+        res.render('menu',{
+          header: 'Menu',
+          menu: MenuJsoon
+        });
+        
+
+});
+});
+
+module.exports = router;
