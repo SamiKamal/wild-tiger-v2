94c854c71c1d3da9518086a8b306bc64c676e33f
Sami <samixgames@hotmail.com>
2019-02-20 22:04:27 +0300

    Create Bar router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var getMenuRouter = require('./routes/getMenu');
+var getBarRouter = require('./routes/getBar');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/',getMenuRouter);
+app.use('/',getBarRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
