e8b69e166085756ce49241fea6e3ba589d2b8b40
Sami <samixgames@hotmail.com>
2019-02-23 13:58:26 +0300

    Add sticky nav to the home page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,24 @@
+// responsive nav
 $('.menu-toggle').click(function(){
 
     $('.nav-ul').toggleClass('nav-open',350);
     $(this).toggleClass('menu-toggle--open')
 
+})
+
+$(document).ready(function () {
+    // sticky nav
+    $('#about-us--js').waypoint(function (direction) {
+        if (direction == 'down'){
+            $('nav').addClass("sticky")
+            $('nav').addClass("animated faster fadeIn")
+        } else {
+            $('nav').removeClass('sticky')
+            $('nav').removeClass("fadeIn")
+            
+
+        }
+    }, {
+        offset: '15%'
+    })
 })
