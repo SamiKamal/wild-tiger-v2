a1316981908040f404d283e217d32533c721fa26
Sami <samixgames@hotmail.com>
2019-02-24 22:56:19 +0300

    Add nav to menu page

---
+++ public/javascripts/menu.js
@@ -0,0 +1,24 @@
+// Respinsive nav
+$('.menu-toggle').click(function(){
+
+    $('.nav-ul').toggleClass('nav-open',350);
+    $(this).toggleClass('menu-toggle--open')
+
+})
+
+//sticky nav
+$(document).ready(function(){
+    $('#menu-section--js').waypoint(function (dir) {
+        if (dir == 'down'){
+            $('nav').addClass("sticky")
+            $('nav').addClass("animated faster fadeIn")
+            $('.header-top').css('display', 'block')
+        } else {
+            $('nav').removeClass('sticky')
+            $('nav').removeClass("fadeIn")
+            $('.header-top').css('display', 'none')        
+
+        }
+    })
+
+})
